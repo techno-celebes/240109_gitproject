d80158fe98914de7fa816998776c401586c5c13a
Techno Celebes <official@techno-celebes.com>
2024-01-09 18:03:52 +0800

    Tambah HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function home(){
+    	echo "Home";
+    }
+
+    public function about(){
+    	echo "About";
+    }
+
+    public function contact(){
+    	echo "About";
+    }
+}
